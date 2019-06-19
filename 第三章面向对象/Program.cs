b15806745ec7f5997ac465d0d6b22d65d66f6d68
wangliming <wangliming@jingshonline.com>
2019-06-19 18:33:22 +0800

    准备编写第五章代码

---
+++ 第三章面向对象/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 封装
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 继承/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 继承
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
